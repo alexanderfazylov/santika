2492dbe16bda3b50bf84d1a9d827f62ab09db130
odenisova <denisovaoj@gmail.com>
2014-10-07 22:35:15 +0400

    subgallery in catalog

--- views/catalog/_line_product_menu_item.php
+++ views/catalog/_line_product_menu_item.php
@@ -41,20 +41,20 @@ use app\models\Upload;
 
                         <div class="subgallery">
                             <ul>
-
                                 <?php foreach ($models as $model): ?>
-                                    <li class="<?= $model->getChilds()->count() > 0 ? 'has_childs' : ''; ?>"
-                                        object_type="<?= $object_type ?>"
-                                        object_id="<?= $model->id ?>"
-                                        hide_level="<?= 'level' . ($level + 1) ?>"
-                                        >
-                                        <a href="<?= $model->urlToFilter(); ?>">
-                                            <?php $src = !empty($model->photo_id) ? $model->photo->getFileShowUrl(Upload::SIZE_SQUARE_245) : Upload::defaultFileUrl(Upload::SIZE_SQUARE_245) ?>
-                                            <div><img src="<?= $src; ?>"></div>
-                                            <?= $model->name; ?>
-                                        </a>
-                                    </li>
+                                    <?php if ($level < 3 && $model->getChilds()->count() > 0): ?>
+                                        <?php
+                                        echo $this->render('/catalog/_line_product_menu_item', array(
+                                            'object_type' => $object_type,
+                                            'parent_type' => $object_type,
+                                            'parent_id' => $model->id,
+                                            'level' => $level + 1,
+                                            'models' => $model->childs,
+                                        ));
+                                        ?>
+                                    <?php endif; ?>
                                 <?php endforeach; ?>
+
 
                             </ul>
 
